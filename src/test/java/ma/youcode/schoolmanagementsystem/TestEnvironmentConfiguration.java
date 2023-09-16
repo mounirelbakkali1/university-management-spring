52ddd35b027dea8e447fa84d8b2ad8835f287ff0
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-16 19:48:12 +0200

    configured test env to use postgresl container

--- src/test/java/ma/youcode/schoolmanagementsystem/TestEnvironmentConfiguration.java
+++ src/test/java/ma/youcode/schoolmanagementsystem/TestEnvironmentConfiguration.java
@@ -1,2 +1,19 @@
-package ma.youcode.schoolmanagementsystem;public class TestEnvironmentConfiguration {
+package ma.youcode.schoolmanagementsystem;
+
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@TestConfiguration
+public class TestEnvironmentConfiguration
+{
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer(){
+        return new PostgreSQLContainer<>("postgres:15");
+    }
+
+
 }
