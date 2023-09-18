b23f76d29a55b94ef08380728923ef856b360e2f
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 13:02:47 +0200

    added controller advice

--- src/main/java/ma/youcode/schoolmanagementsystem/advice/RestExceptionHandler.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/advice/RestExceptionHandler.java
@@ -1,2 +1,21 @@
-package ma.youcode.schoolmanagementsystem.advice;public class RestExceptionHandler {
+package ma.youcode.schoolmanagementsystem.advice;
+
+
+import ma.youcode.schoolmanagementsystem.utils.HttpResponse;
+import ma.youcode.schoolmanagementsystem.utils.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+
+    @ExceptionHandler({NotFoundException.class})
+    public ResponseEntity<HttpResponse> handleNotFoundException(NotFoundException exception) {
+        HttpResponse response = new HttpResponse(exception.getMessage(), 404);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
 }
