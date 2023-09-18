46c4504f6066f3a1a7fd8e87a51ef2f2f42c2c03
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 14:55:24 +0200

    add cours/student controller'

--- src/main/java/ma/youcode/schoolmanagementsystem/controllers/CourseController.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/controllers/CourseController.java
@@ -2,38 +2,70 @@ package ma.youcode.schoolmanagementsystem.controllers;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import ma.youcode.schoolmanagementsystem.dto.CoursDto;
-import ma.youcode.schoolmanagementsystem.entities.Cours;
+import ma.youcode.schoolmanagementsystem.dto.CourseDto;
+import ma.youcode.schoolmanagementsystem.entities.Course;
 import ma.youcode.schoolmanagementsystem.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/courses")
 @Slf4j
-public class CoursController {
+public class CourseController {
 
-    private CourseService courseService;
+    private final CourseService courseService;
 
     @Autowired
-    public CoursController(CourseService courseService){
+    public CourseController(CourseService courseService){
         this.courseService = courseService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cours createCours(@RequestBody @Valid CoursDto course){
+    public Course createCourse(@RequestBody @Valid CourseDto course){
         log.info("Creating new course..");
-        return courseService.addCourse(course);
+        Course addedCourse = courseService.addCourse(course);
+
+        return addedCourse;
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Course>> getAllCourses(){
+        return ResponseEntity.ok(courseService.findAllCourses());
+    }
+
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long courseId){
+        Optional<Course> course = courseService.findCourse(courseId);
+        return course.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 
     @GetMapping
-    public List<Cours> getAllCourses(){
-        return courseService.getAllCourses();
+    public ResponseEntity<Course> getCourseByName(@RequestParam String courseName){
+        Optional<Course> course = courseService.findCourseByName(courseName);
+        return course.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @PutMapping("/{courseId}")
+    public ResponseEntity<Course> updateCourse(@PathVariable Long courseId, @RequestBody CourseDto course){
+        Optional<Course> updatedCourse = courseService.updateCourse(courseId, course);
+        return updatedCourse.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
+    @DeleteMapping("/{courseId}")
+    public ResponseEntity<Course> deleteCourse(@PathVariable Long courseId){
+        Optional<Course> course = courseService.deleteCourse(courseId);
+        return course.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
--- src/main/java/ma/youcode/schoolmanagementsystem/controllers/StudentController.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/controllers/StudentController.java
@@ -1,2 +1,60 @@
-package ma.youcode.schoolmanagementsystem.controllers;public class StudentController {
+package ma.youcode.schoolmanagementsystem.controllers;
+
+import jakarta.validation.Valid;
+import ma.youcode.schoolmanagementsystem.dto.StudentDto;
+import ma.youcode.schoolmanagementsystem.entities.Student;
+import ma.youcode.schoolmanagementsystem.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StudentController {
+
+
+
+
+        private final StudentService studentService;
+
+        @Autowired
+        public StudentController(StudentService studentService){
+            this.studentService = studentService;
+        }
+
+        @PostMapping
+        @ResponseStatus(HttpStatus.CREATED)
+        public ResponseEntity<Student> createStudent(@RequestBody @Valid StudentDto student){
+            return ResponseEntity.ok(studentService.addStudent(student));
+        }
+
+
+        @GetMapping
+        public ResponseEntity<List<StudentDto>> getAllStudents(){
+            return ResponseEntity.ok(studentService.findAllStudents().stream().map(Student::toDto).collect(Collectors.toList()));
+        }
+
+
+        @GetMapping("/{studentId}")
+        public ResponseEntity<Student> getStudentById(@PathVariable Long studentId){
+            Optional<Student> student = studentService.findStudent(studentId);
+            return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
+
+
+        @PutMapping("/{studentId}")
+        public ResponseEntity<Student> updateStudent(@PathVariable Long studentId, @RequestBody StudentDto student){
+            Optional<Student> updatedStudent = studentService.updateStudent(studentId, student);
+            return updatedStudent.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
+
+
+        @DeleteMapping("/{studentId}")
+        public ResponseEntity<Student> deleteStudent(@PathVariable Long studentId){
+            Optional<Student> student = studentService.deleteStudent(studentId);
+            return student.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
 }
