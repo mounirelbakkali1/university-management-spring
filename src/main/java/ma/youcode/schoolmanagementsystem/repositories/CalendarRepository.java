a714cdebabfee386ea8ba407f9bba3600ce224e5
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 14:55:50 +0200

    added JPA repos

--- src/main/java/ma/youcode/schoolmanagementsystem/repositories/CalendarRepository.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/repositories/CalendarRepository.java
@@ -1,2 +1,10 @@
-package ma.youcode.schoolmanagementsystem.repositories;public class CalendarRepository {
+package ma.youcode.schoolmanagementsystem.repositories;
+
+import ma.youcode.schoolmanagementsystem.entities.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 }
--- src/main/java/ma/youcode/schoolmanagementsystem/repositories/CourseRepository.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/repositories/CourseRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import ma.youcode.schoolmanagementsystem.entities.Course;
 
 @Repository
-public interface CoursRepository extends JpaRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    Optional<Course> findCoursByName(String coursName);
+    Optional<Course> findCourseByName(String courseName);
 }
