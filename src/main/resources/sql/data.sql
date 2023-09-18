40ad9de2e7c6f5592bf795a1e13d120cb8011394
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 13:02:06 +0200

    data loading on startup

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -6,8 +6,8 @@ insert into roles (id,name) values (2, 'ROLE_STUDENT');
 insert into roles (id,name) values (3, 'ROLE_PROFESSOR');
 
 
-insert into departements (id,name) values (1, 'Computer Science');
-insert into departements (id,name) values (2, 'Mathematics');
+insert into departments (id,name) values (1, 'Computer Science');
+insert into departments (id,name) values (2, 'Mathematics');
 
 
 insert into professors (id,first_name,last_name,role_id) values (1, 'Ahmed', 'Raissouni', 3);
@@ -18,5 +18,7 @@ insert into students (id,first_name,last_name,role_id) values (1, 'Youssef', 'Ra
 insert into students (id,first_name,last_name,role_id) values (2, 'Yassine', 'Raissouni', 2);
 
 
-insert into academic_calendar(id,start_date,end_date,registration_end_date,holidays) values (1,'2020-09-01','2021-06-30','2020-09-15','2020-12-25,2021-01-01,2021-01-18,2021-02-15,2021-04-02,2021-05-31');
-insert into academic_calendar(id,start_date,end_date,registration_end_date,holidays) values (2,'2020-09-01','2021-06-30','2020-09-15','2020-12-25,2021-01-01,2021-01-18,2021-02-15,2021-04-02,2021-05-31');
+insert into academic_calendar(id,star_date,end_date,registration_end_date,holidays) values (1,'2020-09-01','2021-06-30','2020-09-15',2);
+insert into academic_calendar(id,star_date,end_date,registration_end_date,holidays) values (2,'2020-09-01','2021-06-30','2020-09-15',3);
+
+/*select a1_0.role_id,a1_0.id,a1_0.clazz_,a2_0.id,a2_0.city,a2_0.state,a2_0.street,a2_0.zip_code,a1_0.first_name,a1_0.last_name from (select address_id, id, role_id, first_name, last_name, 0 as clazz_ from app_user union all select address_id, id, role_id, first_name, last_name, 1 as clazz_ from professors union all select address_id, id, role_id, first_name, last_name, 2 as clazz_ from students) a1_0 left join addresses a2_0 on a2_0.id=a1_0.address_id where a1_0.role_id=?
