70d8f122df3ab6eeca120058286c72909fd9b465
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 14:56:16 +0200

    add custom HTTP response pojo

--- src/main/java/ma/youcode/schoolmanagementsystem/utils/HttpResponse.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/utils/HttpResponse.java
@@ -1,2 +1,15 @@
-package ma.youcode.schoolmanagementsystem.utils;public class HttpResponse {
+package ma.youcode.schoolmanagementsystem.utils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+@Builder
+public class HttpResponse {
+    private final String message;
+    private final int status;
+
 }
