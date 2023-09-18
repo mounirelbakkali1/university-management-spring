847b609ad7b48ff0340aab8e654189c909e2b832
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-09-18 13:03:26 +0200

    added global exception handler

--- src/main/java/ma/youcode/schoolmanagementsystem/config/GlobalConfig.java
+++ src/main/java/ma/youcode/schoolmanagementsystem/config/GlobalConfig.java
@@ -1,2 +1,17 @@
-package ma.youcode.schoolmanagementsystem.config;public class GlobalConfig {
+package ma.youcode.schoolmanagementsystem.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalConfig {
+
+
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
